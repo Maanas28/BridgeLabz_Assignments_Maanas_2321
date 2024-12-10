1d76407fecf9e4d9688a833c9148761814afe21b
Maanas28 <maanaskanwar88@gmail.com>
2024-12-10 10:09:08 +0530

    Completed the Array Questions

--- Daily Work Updates/10-12-2024/ArrayQuestions/UnionAndInterserction.js
+++ Daily Work Updates/10-12-2024/ArrayQuestions/UnionAndInterserction.js
@@ -1,6 +1,24 @@
 
 function findIntersection(arr1, arr2) {
-    
+    const intersection = [];
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                // Check if the element is not already in the intersection array
+                let alreadyExists = false;
+                for (let k = 0; k < intersection.length; k++) {
+                    if (intersection[k] === arr1[i]) {
+                        alreadyExists = true;
+                        break;
+                    }
+                }
+                if (!alreadyExists) {
+                    intersection.push(arr1[i]);
+                }
+            }
+        }
+    }
+    return intersection;
 }
 
 
